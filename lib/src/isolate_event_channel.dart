b95f660011163477870bc9200fff1e6061f0d315
Rexios <rexios@rexios.dev>
2025-03-07 23:50:26 -0500

    wip

--- lib/src/isolate_event_channel.dart
+++ lib/src/isolate_event_channel.dart
@@ -2,15 +2,22 @@ import 'dart:async';
 import 'dart:isolate';
 
 import 'package:isolate_channel/isolate_channel.dart';
+import 'package:isolate_channel/src/isolate_message.dart';
 
+/// A channel for receiving events from an isolate
 class IsolateEventChannel {
+  /// The name of the channel
   final String name;
   final SendPort _sendPort;
   final Stream _receivePort;
+  StreamSubscription? _handlerSubscription;
 
   /// Constructor
   IsolateEventChannel(this.name, this._sendPort, this._receivePort);
 
+  /// Receive a broadcast stream of events from the isolate
+  ///
+  /// To be called from the isolate receiving events
   Stream<dynamic> receiveBroadcastStream([dynamic arguments]) {
     final methodChannel = IsolateMethodChannel(name, _sendPort, _receivePort);
     late StreamController<dynamic> controller;
@@ -37,37 +44,67 @@ class IsolateEventChannel {
     return controller.stream;
   }
 
-  void setStreamHandler(IsolateStreamHandler handler) {}
+  /// Set a handler to set up stream handling
+  ///
+  /// To be called from the isolate sending events
+  void setStreamHandler(IsolateStreamHandler? handler) {
+    _handlerSubscription?.cancel();
+    if (handler == null) return;
+
+    _handlerSubscription = _receivePort
+        .where((message) => message is IsolateMessage && message.name == name)
+        .cast<IsolateMessage>()
+        .listen((message) {
+          switch (message.method) {
+            case 'listen':
+              handler.onListen(message.arguments, IsolateEventSink(_sendPort));
+            case 'cancel':
+              handler.onCancel(message.arguments);
+          }
+        });
+  }
 }
 
+/// A handler for setting up stream handling
 abstract class IsolateStreamHandler {
+  /// Called when the stream is listened to
   void onListen(dynamic arguments, IsolateEventSink events);
+
+  /// Called when the stream is canceled
   void onCancel(dynamic arguments);
 }
 
+/// A sink for sending events to the stream
 class IsolateEventSink {
-  /// Create a new [IsolateEventSink] with the given [sink].
-  IsolateEventSink(EventSink<Object?> sink) : _sink = sink;
+  /// Create a new [IsolateEventSink] with the given [SendPort].
+  IsolateEventSink(this._sendPort);
 
-  final EventSink<Object?> _sink;
+  final SendPort _sendPort;
 
   /// Send a success event.
-  void success(Object? event) => _sink.add(event);
+  void success(Object? event) => _sendPort.send(event);
 
   /// Send an error event.
   void error({required String code, String? message, Object? details}) =>
-      _sink.addError(
+      _sendPort.send(
         IsolateExcaption(code: code, message: message, details: details),
       );
 
   /// Send an end of stream event.
-  void endOfStream() => _sink.close();
+  void endOfStream() => _sendPort.send(null);
 }
 
+/// An exception thrown by the isolate
 class IsolateExcaption {
+  /// The code of the exception
   final String code;
+
+  /// The message of the exception
   final String? message;
+
+  /// The details of the exception
   final Object? details;
 
+  /// Constructor
   const IsolateExcaption({required this.code, this.message, this.details});
 }
