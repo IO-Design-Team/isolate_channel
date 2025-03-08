bba259f141387963d54c5dd20e6201f462af50c8
Rexios <rexios@rexios.dev>
2025-03-08 14:57:55 -0500

    Make return from invoke method non-nullable

--- lib/src/isolate_method_channel.dart
+++ lib/src/isolate_method_channel.dart
@@ -15,14 +15,14 @@ class IsolateMethodChannel {
   /// Constructor
   IsolateMethodChannel(this.name, this._sendPort, this._receivePort);
 
-  Future<T?> _invokeMethod<T>(String method, {dynamic arguments}) async {
+  Future<T> _invokeMethod<T>(String method, {dynamic arguments}) async {
     final receivePort = ReceivePort();
     _sendPort.send(
       IsolateMessage(name, method, arguments, receivePort.sendPort),
     );
     final result = await receivePort.first;
     receivePort.close();
-    if (result is T?) {
+    if (result is T) {
       return result;
     } else {
       return Future.error(result);
@@ -30,26 +30,26 @@ class IsolateMethodChannel {
   }
 
   /// Invoke a method on the other isolate
-  Future<T?> invokeMethod<T>(String method, [dynamic arguments]) {
+  Future<T> invokeMethod<T>(String method, [dynamic arguments]) {
     return _invokeMethod<T>(method, arguments: arguments);
   }
 
   /// Invoke a method on the other isolate and return a list
-  Future<List<T>?> invokeListMethod<T>(
+  Future<List<T>> invokeListMethod<T>(
     String method, [
     dynamic arguments,
   ]) async {
     final result = await invokeMethod<List<dynamic>>(method, arguments);
-    return result?.cast<T>();
+    return result.cast<T>();
   }
 
   /// Invoke a method on the other isolate and return a map
-  Future<Map<K, V>?> invokeMapMethod<K, V>(
+  Future<Map<K, V>> invokeMapMethod<K, V>(
     String method, [
     dynamic arguments,
   ]) async {
     final result = await invokeMethod<Map<dynamic, dynamic>>(method, arguments);
-    return result?.cast<K, V>();
+    return result.cast<K, V>();
   }
 
   /// Set a handler to receive method calls from the other isolate
