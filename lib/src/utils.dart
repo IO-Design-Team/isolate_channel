568d807e9b730290843afe6e6ba0db56b9295fe1
Rexios <rexios@rexios.dev>
2025-03-12 15:38:26 -0400

    ...

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -2,10 +2,10 @@ import 'package:isolate_channel/src/model/internal/method_invocation.dart';
 
 /// Extension on [Stream]
 extension StreamExtension on Stream {
-  /// Filter the stream for method invocations for a channel [name]
-  Stream<MethodInvocation> methodInvocations(String name) {
+  /// Filter the stream for method invocations for a [channel]
+  Stream<MethodInvocation> methodInvocations(String channel) {
     return where(
-      (message) => message is MethodInvocation && message.channel == name,
+      (message) => message is MethodInvocation && message.channel == channel,
     ).cast<MethodInvocation>();
   }
 }
